695d1aa14a00827466de9404e5c543954e849de4
Luidson Lima Santos <luidsonlsti@gmail.com>
2026-06-13 15:08:17 -0300

    feat: initialize frontend project and add deployment script for Ministack

---
+++ infra/ministack/deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+ENDPOINT_URL="http://localhost:4566"
+BUCKET_NAME="luidsonl-0shared-front"
+DISTRIBUTION_ID="_0shared_cloudfront"
+
+FRONTEND_DIR="../../frontend"
+
+echo "======================================"
+echo "Starting LocalStack Deploy(Ministack)"
+echo "======================================"
+
+echo "Syncing files from $FRONTEND_DIR to s3://$BUCKET_NAME..."
+aws --endpoint-url=$ENDPOINT_URL s3 sync $FRONTEND_DIR s3://$BUCKET_NAME
+
+echo "Deploy completed successfully!"
