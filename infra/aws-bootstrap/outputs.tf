52a7a5c19f85795e6fd6b04f9fbf96b2a361582d
Luidson Lima Santos <luidsonlsti@gmail.com>
2026-06-20 23:09:58 -0300

    refactor(infra): remove unused DynamoDB table resources for Terraform state locking

--- infra/aws-bootstrap/outputs.tf
+++ infra/aws-bootstrap/outputs.tf
@@ -7,13 +7,3 @@ output "state_bucket_arn" {
   value       = aws_s3_bucket.terraform_state.arn
   description = "S3 bucket ARN for Terraform state"
 }
-
-output "lock_table" {
-  value       = aws_dynamodb_table.terraform_locks.name
-  description = "DynamoDB table name for Terraform state locking"
-}
-
-output "lock_table_arn" {
-  value       = aws_dynamodb_table.terraform_locks.arn
-  description = "DynamoDB table ARN for Terraform state locking"
-}
