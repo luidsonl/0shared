d286058bb99989e8fb6796fd6d69b6f1d2898abe
Luidson Lima Santos <luidsonlsti@gmail.com>
2026-08-04 21:54:48 -0300

    refactor: remove dead letter queue outputs and associated resources from upload queue module

--- terraform/aws-app/resources/modules/upload-queue/main.tf
+++ terraform/aws-app/resources/modules/upload-queue/main.tf
@@ -7,20 +7,10 @@ locals {
 # ---------------------------------------------------------------------------
 # SQS Queues
 # ---------------------------------------------------------------------------
-resource "aws_sqs_queue" "dlq" {
-  name                      = "${var.queue_name}-dlq"
-  message_retention_seconds = 1209600 # 14 days
-}
-
 resource "aws_sqs_queue" "this" {
   name                       = var.queue_name
   visibility_timeout_seconds = 240 # 4 min (Lambda timeout * 6)
   message_retention_seconds  = 1209600 # 14 days
-
-  redrive_policy = jsonencode({
-    deadLetterTargetArn = aws_sqs_queue.dlq.arn
-    maxReceiveCount     = 3
-  })
 }
 
 # ---------------------------------------------------------------------------
--- terraform/aws-app/resources/modules/upload-queue/outputs.tf
+++ terraform/aws-app/resources/modules/upload-queue/outputs.tf
@@ -6,14 +6,6 @@ output "queue_arn" {
   value = aws_sqs_queue.this.arn
 }
 
-output "dlq_url" {
-  value = aws_sqs_queue.dlq.id
-}
-
-output "dlq_arn" {
-  value = aws_sqs_queue.dlq.arn
-}
-
 output "lambda_function_name" {
   value = aws_lambda_function.registration.function_name
 }
